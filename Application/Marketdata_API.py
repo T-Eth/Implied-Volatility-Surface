3a6f2ca132351da331bddda627875e9c8fff26e0
Tyler Etherton <=>
2024-12-13 16:00:32 +0800

    Fixed bugs in Marketdata_API and Surface prevented surface from trying to build a surface with highly sporadic data, limited options to be considered to only those divisible by 10 as they are more actively traded and so have markets more fully across the range of dte. Added ivdata.csv to allow for easier viewing of the data taken from the api.

--- Application/Marketdata_API.py
+++ Application/Marketdata_API.py
@@ -4,19 +4,19 @@ import json
 import pandas as pd
 
 class optionsChain:
-    def __init__(self, underlyingSymbol='AAPL', strikeFrom=100, strikeTo=100, side='call'):
+    def __init__(self, underlyingSymbol='AAPL', strikeFrom=80, strikeTo=120, side='call'):
         self.underlyingSymbol = underlyingSymbol
         self.side = side
         #Get options expiring from today to two years from now
         self.dateFrom = datetime.today().date().isoformat()
-        self.dateTo = datetime.today().date().replace(year=datetime.today().date().year+2).isoformat()
+        self.dateTo = datetime.today().date().replace(year=datetime.today().date().year+1).isoformat()
         #Select range of strikes based on last traded price
         self.quoteUrl = 'https://api.marketdata.app/v1/stocks/quotes/AAPL/'
         self.quoteResponse = requests.request("GET", self.quoteUrl)
         self.quote = json.loads(self.quoteResponse.text)
         self.strikeFrom = round(self.quote['last'][0]*strikeFrom/100)
         self.strikeTo = round(self.quote['last'][0]*strikeTo/100)
-        self.url = f'https://api.marketdata.app/v1/options/chain/{underlyingSymbol}/?strike={strikeFrom}-{strikeTo}&from={self.dateFrom}&to={self.dateTo}&side={self.side}'
+        self.url = f'https://api.marketdata.app/v1/options/chain/{underlyingSymbol}/?strike={self.strikeFrom}-{self.strikeTo}&from={self.dateFrom}&to={self.dateTo}&side={self.side}'
         self.get_access_token()
         self.headers = {
                             'Accept': 'application/json',
@@ -24,6 +24,7 @@ class optionsChain:
                         }
         self.chain = requests.request("GET", self.url, headers=self.headers)
         self.optionsChain = pd.DataFrame(json.loads(self.chain.text))
+        self.optionsChain = self.optionsChain.where(self.optionsChain['strike']%10 == 0).dropna()
 
     def get_access_token(self):
         with open('md.token', 'r') as file:
@@ -32,7 +33,8 @@ class optionsChain:
 def main():
     chain = optionsChain()
     print(chain.optionsChain.head())
-    print(chain.optionsChain['iv'])
+    print(chain.optionsChain.tail())
+    print(chain.optionsChain['iv'].head())
 
 if __name__ == '__main__':
     main()
--- Application/Surface.py
+++ Application/Surface.py
@@ -3,24 +3,32 @@ import pandas as pd
 import plotly.graph_objects as go
 
 class Surface:
-    def __init__(self, underlyingSymbol='AAPL', strikeFrom=80, strikeTo=120, side='call'):
+    def __init__(self, underlyingSymbol='AAPL', strikeFrom=70, strikeTo=130, side='call'):
         self.chain = optionsChain(underlyingSymbol, strikeFrom, strikeTo, side)
         self.plot_surface()
 
     def plot_surface(self):
+        print(self.chain.optionsChain.head())
+        print(self.chain.optionsChain.tail())
         surface_df = self.chain.optionsChain.pivot(index='dte', columns='strike', values='iv')
         surface = go.Figure(data=[
                                     go.Surface(z=surface_df.values,
-                                    x=surface_df.columns,
-                                    y=surface_df.index)])
+                                    x=surface_df.index,
+                                    y=surface_df.columns)])
+        surface.update_layout(title='Implied Volatility Surface',
+                                scene = dict(
+                                                xaxis_title='Days to Expiration',
+                                                yaxis_title='Strike',
+                                                zaxis_title='Implied Volatility'))
+        print("Before writing CSV")
+        print(surface_df.index)
+        surface_df.to_csv('ivdata.csv', index=True)
+        print("After writing CSV")
+        print(surface_df.index) 
         surface.show()
 
 def main():
     surface = Surface()
-    print(surface.chain.optionsChain.head())
-    print(surface.chain.optionsChain['iv'].head())
-    print(surface.chain.optionsChain['dte'].head())
-    print(surface.chain.optionsChain['strike'].head())
 
 if __name__ == '__main__':
     main()
