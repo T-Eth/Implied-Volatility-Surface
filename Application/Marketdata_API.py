21405aebe097287ab6eb0510cceec16909422fdd
Tyler Etherton <=>
2024-12-13 15:01:29 +0800

    Added ability to select side of market data adjusted API call to request expirations from 0 days up to 2 years.

--- Application/Marketdata_API.py
+++ Application/Marketdata_API.py
@@ -4,20 +4,19 @@ import json
 import pandas as pd
 
 class optionsChain:
-    def __init__(self, underlyingSymbol='AAPL', strikeFrom=100, strikeTo=100):
+    def __init__(self, underlyingSymbol='AAPL', strikeFrom=100, strikeTo=100, side='call'):
         self.underlyingSymbol = underlyingSymbol
-        
+        self.side = side
         #Get options expiring from today to two years from now
         self.dateFrom = datetime.today().date().isoformat()
-        #self.dateTo = datetime.today().date().replace(year=datetime.today().date().year+2).isoformat()
-        self.dateTo = datetime.today().date().isoformat()
+        self.dateTo = datetime.today().date().replace(year=datetime.today().date().year+2).isoformat()
         #Select range of strikes based on last traded price
         self.quoteUrl = 'https://api.marketdata.app/v1/stocks/quotes/AAPL/'
         self.quoteResponse = requests.request("GET", self.quoteUrl)
         self.quote = json.loads(self.quoteResponse.text)
         self.strikeFrom = round(self.quote['last'][0]*strikeFrom/100)
         self.strikeTo = round(self.quote['last'][0]*strikeTo/100)
-        self.url = f'https://api.marketdata.app/v1/options/chain/{underlyingSymbol}/?strike={strikeFrom}-{strikeTo}&from={self.dateFrom}&to={self.dateTo}/'
+        self.url = f'https://api.marketdata.app/v1/options/chain/{underlyingSymbol}/?strike={strikeFrom}-{strikeTo}&from={self.dateFrom}&to={self.dateTo}&side={self.side}'
         self.get_access_token()
         self.headers = {
                             'Accept': 'application/json',
@@ -33,6 +32,7 @@ class optionsChain:
 def main():
     chain = optionsChain()
     print(chain.optionsChain.head())
+    print(chain.optionsChain['iv'])
 
 if __name__ == '__main__':
     main()
